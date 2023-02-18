2155e8a9f7cd6af7db6af6c275eab2f62f9a37b5
SamuelCheng123 <samuel.yhcheng@gmail.com>
2023-02-19 02:03:10 +0800

    hardhatconfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,77 +1,89 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-etherscan";
-
+import "hardhat-gas-reporter";
 // import "@openzeppelin/hardhat-defender";
 
 require("dotenv").config();
 import "./tasks";
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.address);
-  }
-});
-
+const {
+  DEPLOYER_PRIVATE_KEY,
+  TOKEN_VAULT_PRIVATE_KEY,
+  SALES_VAULT_PRIVATE_KEY,
+} = process.env;
+if (
+  !DEPLOYER_PRIVATE_KEY ||
+  !TOKEN_VAULT_PRIVATE_KEY ||
+  !SALES_VAULT_PRIVATE_KEY
+) {
+  throw new Error("Missing ENV Key");
+}
+const accounts = [
+  DEPLOYER_PRIVATE_KEY,
+  TOKEN_VAULT_PRIVATE_KEY,
+  SALES_VAULT_PRIVATE_KEY,
+];
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   solidity: {
-      version: "0.8.7",
-      settings: {
-          optimizer: {
-              enabled: true,
-              runs: 200
-          }
-      }
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
   },
   networks: {
-      localhost: {
-          url: "http://127.0.0.1:8545"
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+      accounts,
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_GOERLI_API_KEY}`,
+      accounts,
+    },
+    polygon: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_MATIC_API_KEY}`,
+      accounts,
+    },
+    polygonMumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_MUMBAI_API_KEY}`,
+      // gasPrice: 100000000000,
+      accounts,
+    },
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      mining: {
+        auto: true,
+        interval: [13 * 1000, 15 * 1000],
       },
-      mainnet: {
-          url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
-          accounts: process.env.DEV_PRIVATE_KEY !== undefined ? [process.env.DEV_PRIVATE_KEY] : [],
-          gasPrice: 200000000000,
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+        blockNumber: 12772572,
       },
-      rinkeby: {
-          url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_RINKEBY_API_KEY}`,
-          accounts: process.env.DEV_PRIVATE_KEY !== undefined ? [process.env.DEV_PRIVATE_KEY] : [],
-      },
-      matic:{
-          url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_RINKEBY_API_KEY}`,
-          accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
-      },
-      mumbai:{
-        url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_RINKEBY_API_KEY}`,
-        accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
-      },
-
-
-      hardhat: {
-        allowUnlimitedContractSize: true,
-        mining: {
-          auto: true,
-          interval: [13 * 1000, 15 * 1000],
-        },
-        forking: {
-          url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
-          blockNumber: 12772572,
-        },
-      },
+    },
   },
   mocha: {
-      timeout: 100000
+    timeout: 100000,
   },
   etherscan: {
-      apiKey: process.env.ETHERSCAN_API,
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY
+        ? process.env.ETHERSCAN_API_KEY
+        : "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY
+        ? process.env.POLYGONSCAN_API_KEY
+        : "",
+    },
   },
-}
-
+};
 
 export default config;
